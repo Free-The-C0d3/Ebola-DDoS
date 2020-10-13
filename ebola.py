fe3119724034baa7c345d00b9a95a9961e99156a
413xPr06605 <70262451+413xPr06605@users.noreply.github.com>
2020-10-13 16:33:45 +0800

    Edit Some Information

--- ebola.py
+++ ebola.py
@@ -19,7 +19,7 @@ else:
 
 print("""
       ▓█████  ▄▄▄▄    ▒█████   ██▓    ▄▄▄   Ebola DDoS Tool Code By 413xPr06605.
-3     ▓█   ▀ ▓█████▄ ▒██▒  ██▒▓██▒   ▒████▄         This Version Only For Linux.
+3     ▓█   ▀ ▓█████▄ ▒██▒  ██▒▓██▒   ▒████▄         Windows/Linux Supported Ver.
  1    ▒███   ▒██▒ ▄██▒██░  ██▒▒██░   ▒██  ▀█▄                Socks4/5 Supported.
 8 4   ▒▓█  ▄ ▒██░█▀  ▒██   ██░▒██░   ░██▄▄▄▄██                Version ~ # 1.5.0,
  2    ░▒████▒░▓█  ▀█▓░ ████▓▒░░██████▒▓█   ▓██▒
